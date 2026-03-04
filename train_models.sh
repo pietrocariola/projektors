fc1cd3fe5a577f3b061c39a130ad48ea1f3156a6
Pietro <pietrocariola@gmail.com>
2026-03-04 17:44:39 +0000

    add train script

---
+++ train_models.sh
@@ -0,0 +1,11 @@
+python -m projektors.llava-agri.mlp.train
+python -m projektors.llava-derm.mlp.train
+
+python -m projektors.llava-agri.transformer.train
+python -m projektors.llava-derm.transformer.train
+
+python -m projektors.instructblip-agri.mlp.train
+python -m projektors.instructblip-derm.mlp.train
+
+python -m projektors.instructblip-agri.transformer.train
+python -m projektors.instructblip-derm.transformer.train
