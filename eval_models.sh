bc453b400abafa4b0bdea177307c1f8149ee0b2a
Pietro <pietrocariola@gmail.com>
2026-03-04 17:51:04 +0000

    add scripts

---
+++ eval_models.sh
@@ -0,0 +1,23 @@
+python -m projektors.llava-agri.mlp.test_pre
+python -m projektors.llava-agri.mlp.test_post
+
+python -m projektors.llava-derm.mlp.test_pre
+python -m projektors.llava-derm.mlp.test_post
+
+python -m projektors.llava-agri.transformer.test_pre
+python -m projektors.llava-agri.transformer.test_post
+
+python -m projektors.llava-derm.transformer.test_pre
+python -m projektors.llava-derm.transformer.test_post
+
+python -m projektors.instructblip-agri.mlp.test_pre
+python -m projektors.instructblip-agri.mlp.test_post
+
+python -m projektors.instructblip-derm.mlp.test_pre
+python -m projektors.instructblip-derm.mlp.test_post
+
+python -m projektors.instructblip-agri.transformer.test_pre
+python -m projektors.instructblip-agri.transformer.test_post
+
+python -m projektors.instructblip-derm.transformer.test_pre
+python -m projektors.instructblip-derm.transformer.test_post
